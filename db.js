eaf1e4cb9d4c56ab0f4c76199ecd180f2eafa020
Esmyrna <103766811+Esmyrna@users.noreply.github.com>
2022-12-03 18:38:07 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg')
 
-// Criando pool de conexão:
 const pool = new Pool({
     connectionString: 'postgres://llfwzkms:a2EImct7DEtfzbRDz606uyvzmYzezZ9g@babar.db.elephantsql.com/llfwzkms'
 })
@@ -8,14 +7,12 @@ const pool = new Pool({
 
 const SelecionarTarefas = async (_request, response) => {
 
-    // Query de SELECT:
     pool.query('SELECT * FROM tasks', (error, results) => {
 
-        // Lançando erro:
         if (error) {
           throw error
         }
-        // Response com as linhas do sql
+     
         response.status(200).json(results.rows)
       })
     
